bfa27f88d6bd26288504a099b76e79b09c9df9bf
Gaurav Pokharna <gaurav.pokharna@neosofttech.com>
2020-05-13 14:53:34 +0530

    Updates

---
+++ precache-manifest.374d426add17733e54b61ff55023af91.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df98fb6ee8a77c26df559720f3fcd215",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "8f25ad5dfeda43e465cc",
+    "url": "/monster-rolodex/static/css/main.6bca4f18.chunk.css"
+  },
+  {
+    "revision": "1b163c6dfb305b2c587f",
+    "url": "/monster-rolodex/static/js/2.b0e7918a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.b0e7918a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f25ad5dfeda43e465cc",
+    "url": "/monster-rolodex/static/js/main.5bc31037.chunk.js"
+  },
+  {
+    "revision": "1db901b49b16624dee18",
+    "url": "/monster-rolodex/static/js/runtime-main.b410029c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/monster-rolodex/static/media/logo.5d5d9eef.svg"
+  }
+]);
